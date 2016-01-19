6d6ed6f12bcfdbf3bfc64d351d507a8f5b8f74f6
princeofdarkness76 <djsteviejames@hotmail.com>
2016-01-19 13:36:33 -0800

    Merge remote-tracking branch 'origin/yosuke-patch-1' into pod/node
    # Conflicts:
    #	test/parallel/test-querystring-multichar-separator.js

--- test/parallel/test-querystring-multichar-separator.js
+++ test/parallel/test-querystring-multichar-separator.js
@@ -1,24 +1,44 @@
 'use strict';
+<<<<<<< HEAD
 require('../common');
+=======
+const common = require('../common');
+>>>>>>> origin/yosuke-patch-1
 const assert = require('assert');
 const qs = require('querystring');
 
 assert.deepEqual(
+<<<<<<< HEAD
   qs.parse('foo=>bar&&bar=>baz', '&&', '=>'),
+=======
+  qs.parse('foo=>bar&&bar=>baz', '&&', '=>'), 
+>>>>>>> origin/yosuke-patch-1
   {foo: 'bar', bar: 'baz'}
 );
 
 assert.strictEqual(
+<<<<<<< HEAD
   qs.stringify({foo: 'bar', bar: 'baz'}, '&&', '=>'),
+=======
+  qs.stringify({foo: 'bar', bar: 'baz'}, '&&', '=>'), 
+>>>>>>> origin/yosuke-patch-1
   'foo=>bar&&bar=>baz'
 );
 
 assert.deepEqual(
+<<<<<<< HEAD
   qs.parse('foo==>bar, bar==>baz', ', ', '==>'),
+=======
+  qs.parse('foo==>bar, bar==>baz', ', ', '==>'), 
+>>>>>>> origin/yosuke-patch-1
   {foo: 'bar', bar: 'baz'}
 );
 
 assert.strictEqual(
+<<<<<<< HEAD
   qs.stringify({foo: 'bar', bar: 'baz'}, ', ', '==>'),
+=======
+  qs.stringify({foo: 'bar', bar: 'baz'}, ', ', '==>'), 
+>>>>>>> origin/yosuke-patch-1
   'foo==>bar, bar==>baz'
 );
